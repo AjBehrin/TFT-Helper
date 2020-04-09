d225fca737d806501c389d11d120ed720e63bd9f
Alex Behringer <Ajbehrin@gmail.com>
2020-04-09 09:42:20 -0500

    Initial Testing
    Not much here yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/ChampIcon';
+import ChampIcon from './components/ChampIcon';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="Champ-Flex">
+            <ChampIcon />
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <p>Worlds Best TFT Planner</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ChampIcon.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function ChampIcon() {
+
+    const champIcons = ['airicon.png', 'fireicon.png', 'watericon.png', 'earthicon.png'];
+
+    return (
+        <ul className="flex-container">
+            
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+            <li><img src="images/fireicon.png" alt="" className="flex-champ" /></li>
+              
+            
+        </ul>
+    );
+}
+
+export default ChampIcon;
